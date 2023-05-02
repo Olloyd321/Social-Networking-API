8036700885268fe7dbc7551a2605bc1a51b38c8f
Ollie Lloyd <olloyd321@gmail.com>
2023-05-02 18:35:42 -0400

    added more lines to models

--- models/users.js
+++ models/users.js
@@ -7,10 +7,31 @@ const usersSchema = new  mongoose.Schema(
         required: true,
         unique: true,
         trimmed: true,
+        },
 
-        }
+        email:{
+            type: String,
+            required: true,
+            unique: true,
+
+        },
+
+        thoughts:{
+
+        },
+
+        friends:{
+
+        },
     }
 );
+
+usersSchema
+.virtual('')
+// getter
+.get(function () {
+    return
+})
 
 const User = mongoose.model('User', usersSchema)
 modeule.exports = User;
